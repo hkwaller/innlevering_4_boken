ee542fc6aabed35257c484910f4fc14dd51d39db
Hannes Waller <hkwaller@gmail.com>
2015-05-29 19:33:13 +0200

    server tests working

--- test/server/controllers/api/albums.spec.js
+++ test/server/controllers/api/albums.spec.js
@@ -1,6 +1,9 @@
-var expect =            require('chai').expect
-var ctrl =              require('../../../../server/controllers/albums'),
-    api =               require('../../support/api')
+var expect =            require('chai').expect,
+    ctrl =              require('../../../../server/controllers/albums'),
+    api =               require('../../support/api'),
+    Album =             require('../../../../server/models/album'),
+    user =              require('../../support/user')    
+    
 
 describe('controllers.albums', function() {
     it('exists', function() {
@@ -9,11 +12,58 @@ describe('controllers.albums', function() {
 })
 
 describe('controllers.api.albums', function() {
-    describe('GET /api/albums', function() {
-        it('exists', function(done) {
-            api.get('/api/albums/')
+    
+    describe('POST /api/albums', function() {
+        var token
+        
+        beforeEach(function(done) {
+            Album.remove({}, function(err) {
+                if (err) console.log(err)
+            })
+
+            user.create('test', 'test', function(err, user) {
+                token = user.token
+                done(err)
+            })
+        })
+        
+        beforeEach(function(done) {
+            api.post('/albums')
+                .send({ title: 'Album', artist: 'Artist', creator: 'Creator', public: true })
+                .set('X-Auth', token)
                 .expect(200)
                 .end(done)
         })
+        
+        it('added one album', function(done) {
+            Album.findOne(function(err, album) {
+                expect(album.title).to.equal('Album')
+                done(err)
+            })
+        })
+        
+        
+    })
+    
+    describe('GET /api/albums', function() {
+        var token
+        
+        beforeEach(function(done) {
+            user.create('test', 'test', function(err, user) {
+                token = user.token
+                done(err)
+            })
+        })
+        
+        it('has one album', function(done) {
+            api.get('/albums')
+                .set('X-Auth', token)
+                .expect(200)
+                .expect(function (response) {
+                    expect(response.body).to.have.length(1)
+                })
+                .end(done)
+        })
+    
     })
 })
---
+++ test/server/support/user.js
@@ -0,0 +1,24 @@
+var bcrypt =        require('bcrypt'),
+    jwt =           require('jsonwebtoken'),
+    config =        require('../../../server/secrets'),
+    User =          require('../../../server/models/user')
+
+exports.create = function(username, password, cb) {
+    var user = new User({username: username})
+    
+    bcrypt.hash(password, 10, function(err, hash) {
+        if (err) return cb(err)
+        
+        user.password = hash
+        user.save(function(err) {
+            if (err) return cb(err)
+            
+            user.token = jwt.sign({
+                username: user.username
+            }, config.jwt)
+            
+            cb(null, user)
+        })
+    })
+    
+}
